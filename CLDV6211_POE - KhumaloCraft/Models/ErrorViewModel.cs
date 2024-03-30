83e7912df51daa004bb26346492771f8037b506c
Melissa Govender <melissa.govender@derivco.co.za>
2024-03-30 20:59:19 +0200

    Add project files.

---
+++ CLDV6211_POE - KhumaloCraft/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CLDV6211_POE___KhumaloCraft.Models
+{
+  public class ErrorViewModel
+  {
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
